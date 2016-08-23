0701c9e44c40ec4a98ffc96f11c2b10261a92b70
stsyn <stasyan1902@gmail.com>
2016-08-23 13:08:45 +0500

    cleaning

--- CRF/Modules/Binaries/Main.h
+++
@@ -1,8 +0,0 @@
-#ifndef BINAR_MAIN_H_INCLUDED
-#define BINAR_MAIN_H_INCLUDED
-
-	void FindTransparencyBorder(int id, int resid,unsigned int msc);
-	void Overlay(int id, int color);
-	void Smooth(int id, int v,unsigned int msc);
-
-#endif
